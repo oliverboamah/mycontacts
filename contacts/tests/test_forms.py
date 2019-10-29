75e8f9f47f86b589da88f2facf95538d0d14d9f3
Oliver Boamah <oliverboamah@yahoo.com>
2019-10-29 16:14:21 +0000

    Add tests to ensure update contact form accepts valid data

--- contacts/tests/test_forms.py
+++ contacts/tests/test_forms.py
@@ -1,31 +1,32 @@
 from django.test import TestCase
 from contacts.forms.create_contact_form import CreateContactForm
+from contacts.forms.update_contact_form import UpdateContactForm
 
 
 class ContactFormTestCase(TestCase):
+    valid_form_data = {
+        'phone_number': '0234110022',
+        'name': 'Ronaldo',
+        'email': 'ronaldo@yahoo.com'
+    }
+
+    invalid_form_data = {
+        'phone_number': '0234110022',
+        'name': 'Ronaldo',
+        'email': 'ronaldo'
+    }
 
     def test_create_contact_form_accepts_valid_data(self):
-        valid_form_data = {
-            'phone_number': '0234110022',
-            'name': 'Ronaldo',
-            'email': 'ronaldo@yahoo.cm'
-        }
-
-        create_contact_form = CreateContactForm(data=valid_form_data)
+        create_contact_form = CreateContactForm(data=self.valid_form_data)
         self.assertEqual(create_contact_form.is_valid(), True)
 
     def test_create_contact_form_rejects_valid_data(self):
-        invalid_form_data = {
-            'phone_number': '0234110022',
-            'name': 'Ronaldo',
-            'email': 'ronaldo'
-        }
-
-        create_contact_form = CreateContactForm(data=invalid_form_data)
+        create_contact_form = CreateContactForm(data=self.invalid_form_data)
         self.assertEqual(create_contact_form.is_valid(), False)
 
     def test_update_contact_form_accepts_valid_data(self):
-        pass
+        update_contact_form = UpdateContactForm(data=self.valid_form_data)
+        self.assertEqual(update_contact_form.is_valid(), True)
 
     def test_update_contact_form_rejects_valid_data(self):
         pass
